a54f0df3d8522e45b55c436da69ba0f82db84151
Harshal D Ganbote <harshalganbote55@gmail.com>
2026-06-02 11:05:32 +0530

    Add transfer endpoint and currency validation

---
+++ api/validator.go
@@ -0,0 +1,14 @@
+package api
+
+import (
+	"github.com/go-bank/internal/utils"
+	"github.com/go-playground/validator/v10"
+)
+
+var validCurrency validator.Func = func(fl validator.FieldLevel) bool {
+	if currency, ok := fl.Field().Interface().(string); ok {
+		return utils.IsSupportCurrency(currency)
+	}
+
+	return false
+}
