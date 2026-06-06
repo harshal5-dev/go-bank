b003d2d6332996af096432d9498abd14e235d1de
Harshal D Ganbote <harshalganbote55@gmail.com>
2026-06-06 22:05:10 +0530

    Refactor server initialization and add login user functionality with tests

--- api/user.go
+++ api/user.go
@@ -1,11 +1,13 @@
 package api
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	db "github.com/go-bank/internal/db/sqlc"
 	"github.com/go-bank/internal/utils"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgtype"
 )
@@ -17,12 +19,32 @@ type createUserRequest struct {
 	Email    string `json:"email" binding:"required,email"`
 }
 
-type createUserResponse struct {
+type userResponse struct {
 	Username          string             `json:"username"`
 	FullName          string             `json:"full_name"`
 	Email             string             `json:"email"`
 	PasswordChangedAt pgtype.Timestamptz `json:"password_changed_at"`
 	CreatedAt         pgtype.Timestamptz `json:"created_at"`
+}
+
+type loginUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+}
+
+type loginUserResponse struct {
+	AccessToken string       `json:"access_token"`
+	User        userResponse `json:"user"`
+}
+
+func newUserResponse(user db.User) userResponse {
+	return userResponse{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: user.PasswordChangedAt,
+		CreatedAt:         user.CreatedAt,
+	}
 }
 
 func (server *Server) createUser(ctx *gin.Context) {
@@ -60,38 +82,45 @@ func (server *Server) createUser(ctx *gin.Context) {
 		return
 	}
 
-	res := createUserResponse{
-		Username:          createdUser.Username,
-		FullName:          createdUser.FullName,
-		Email:             createdUser.Email,
-		PasswordChangedAt: createdUser.PasswordChangedAt,
-		CreatedAt:         createdUser.CreatedAt,
-	}
+	res := newUserResponse(createdUser)
 
 	ctx.JSON(http.StatusCreated, res)
 }
 
-// type getUserRequest struct {
-// 	Username string `json:"username" binding:"required,alphanum"`
-// }
+func (server *Server) loginUser(ctx *gin.Context) {
+	var req loginUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
 
-// func (server *Server) getUser(ctx *gin.Context) {
-// 	var req getAccountRequest
-// 	if err := ctx.ShouldBindUri(&req); err != nil {
-// 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-// 		return
-// 	}
+	user, err := server.store.GetUser(ctx, req.Username)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
 
-// 	fetchedAccount, err := server.store.GetAccount(ctx, req.ID)
-// 	if err != nil {
-// 		if err == sql.ErrNoRows {
-// 			ctx.JSON(http.StatusNotFound, errorResponse(err))
-// 			return
-// 		}
+			ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+			return
+	}
 
-// 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-// 		return
-// 	}
+	err = utils.CheckPassword(req.Password, user.HashPassword)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+			return
+	}
 
-// 	ctx.JSON(http.StatusOK, fetchedAccount)
-// }
+	accessToken, err := server.tokenMaker.CreateToken(user.Username, server.config.AccessTokenDuration)
+		if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+			return
+	}
+
+	rsp := loginUserResponse {
+		AccessToken: accessToken,
+		User: newUserResponse(user),
+	}
+
+	ctx.JSON(http.StatusOK, rsp)
+}
